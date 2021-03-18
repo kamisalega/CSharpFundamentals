771dd8cb0cb0efa71c909fe34656f4149ccee7e7
Kamil <kamilsalega@gmail.com>
2021-03-18 18:53:10 +0100

    Tworzenie zwyczjanego delegata

--- LambdasAndDelegates/DelegatesAndEvents/DelegatesAndEvents/Program.cs
+++ LambdasAndDelegates/DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
     class Program
     {
+
         static void Main(string[] args)
         {
+            var del1 = new WorkPerformedHandler(WorkPerformed1);
+            var del2 = new WorkPerformedHandler(WorkPerformed2);
+
+            DoWork(del2);
+
+            Console.Read();
         }
+
+        static void DoWork(WorkPerformedHandler del)
+        {
+            del(5, WorkType.GoToMeetings);
+        }
+
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed1 called" + hours.ToString());
+        }
+        static void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed1 called" + hours.ToString());
+        }
+    }
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
     }
 }
