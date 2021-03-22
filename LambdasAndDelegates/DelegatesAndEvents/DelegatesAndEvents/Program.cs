5616cf71280ada3be29d824df0bcd5d46d476d40
Kamil <kamilsalega@gmail.com>
2021-03-22 22:22:43 +0100

    Zwracanie wartości za pomocą delegata

--- LambdasAndDelegates/DelegatesAndEvents/DelegatesAndEvents/Program.cs
+++ LambdasAndDelegates/DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -6,18 +6,32 @@ using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
-    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+    public delegate int WorkPerformedHandler(int hours, WorkType workType);
+
     class Program
     {
-
         static void Main(string[] args)
         {
             var del1 = new WorkPerformedHandler(WorkPerformed1);
             var del2 = new WorkPerformedHandler(WorkPerformed2);
+            var del3 = new WorkPerformedHandler(WorkPerformed3);
+
+            del1 += del2 + del3;
+
+
+            int finalHours = del1(10, WorkType.GenerateReports);
+            Console.WriteLine(finalHours);
 
             DoWork(del2);
 
             Console.Read();
+        }
+
+        private static int WorkPerformed3(int hours, WorkType worktype)
+        {
+            Console.WriteLine("WorkPerformed3 called" + hours.ToString());
+
+            return hours + 3;
         }
 
         static void DoWork(WorkPerformedHandler del)
@@ -25,13 +39,17 @@ namespace DelegatesAndEvents
             del(5, WorkType.GoToMeetings);
         }
 
-        static void WorkPerformed2(int hours, WorkType workType)
+        static int WorkPerformed2(int hours, WorkType workType)
         {
-            Console.WriteLine("WorkPerformed1 called" + hours.ToString());
+            Console.WriteLine("WorkPerformed2 called" + hours.ToString());
+
+            return hours + 2;
         }
-        static void WorkPerformed1(int hours, WorkType workType)
+        static int WorkPerformed1(int hours, WorkType workType)
         {
             Console.WriteLine("WorkPerformed1 called" + hours.ToString());
+
+            return hours + 1;
         }
     }
 
