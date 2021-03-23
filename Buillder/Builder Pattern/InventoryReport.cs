275a52617a0a0ca2fa732a3b73a6d9a5bbc970cf
Kamil <kamilsalega@gmail.com>
2021-03-23 18:45:48 +0100

    Wdrożenie konkeretnych builderów

--- Buillder/Builder Pattern/InventoryReport.cs
+++ Buillder/Builder Pattern/InventoryReport.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Builder_Pattern
@@ -42,9 +43,51 @@ namespace Builder_Pattern
     {
         void AddTitle();
         void AddDimensions();
-        void AddLogistics();
-
+        void AddLogistics(DateTime dateTime);
         InventoryReport GetDailyReport();
+    }
 
+    public class DailyReportBuilder : IFurnitureInventoryBuilder
+    {
+        private InventoryReport _report;
+        private IEnumerable<FurnitureItem> _items;
+
+        public DailyReportBuilder(IEnumerable<FurnitureItem> items)
+        {
+            Reset();
+            _items = items;
+        }
+
+
+        public void AddTitle()
+        {
+            _report.TitleSection = "---- Daily Inventory Report ----\n";
+        }
+
+        public void AddDimensions()
+        {
+            _report.DimensionsSection = string.Join(Environment.NewLine,
+                _items.Select(product => $"Product: {product.Name} \nPrice: {product.Price} \n" +
+                                         $"Height: {product.Height} x Width: {product.Width} " +
+                                         $"-> Weight: {product.Weight} kg"));
+        }
+
+        public void AddLogistics(DateTime dateTime)
+        {
+            _report.LogisticsSection = $"Report generated on {dateTime}";
+        }
+
+        public InventoryReport GetDailyReport()
+        {
+            InventoryReport finishedReport = _report;
+            Reset();
+
+            return finishedReport;
+        }
+
+        public void Reset()
+        {
+            _report = new InventoryReport();
+        }
     }
 }
