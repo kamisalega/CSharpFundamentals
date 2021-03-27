8cf475b0f09dabfbbf2d252a213ebdecd0f9755d
Kamil <kamilsalega@gmail.com>
2021-03-27 20:14:13 +0100

    Initial Project

---
+++ Mediator Pattern/Mediator Pattern/Colleague.cs
@@ -0,0 +1,22 @@
+﻿namespace Mediator_Pattern
+{
+    public abstract class Colleague
+    {
+        protected Mediator mediator;
+
+        public Colleague(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+        internal void SetMediator(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public virtual void Send(string message)
+        {
+            this.mediator.Send(message, this);
+        }
+        public abstract void HandleNotification(string message);
+    }
+}
