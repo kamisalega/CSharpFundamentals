6aad97ee7b326039adc2b2ed88272850ba5b384f
Kamil <kamilsalega@gmail.com>
2021-03-27 20:35:39 +0100

    Dodanie klasy "Director"

--- Buillder/Builder Pattern/InventoryReport.cs
+++ Buillder/Builder Pattern/InventoryReport.cs
@@ -90,4 +90,21 @@ namespace Builder_Pattern
             _report = new InventoryReport();
         }
     }
+
+    public class InventoryBuildDirector
+    {
+        private IFurnitureInventoryBuilder _builder;
+
+        public InventoryBuildDirector(IFurnitureInventoryBuilder concreteBuilder)
+        {
+            _builder = concreteBuilder;
+        }
+
+        public void BuildCompleteReport()
+        {
+            _builder.AddTitle();
+            _builder.AddDimensions();
+            _builder.AddLogistics(DateTime.Now);
+        }
+    }
 }
