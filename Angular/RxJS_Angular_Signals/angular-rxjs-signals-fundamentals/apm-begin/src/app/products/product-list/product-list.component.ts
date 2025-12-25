86edd9a16c5e0f904992b79f7871070da53640e9
Kamil Salega <kamilsalega@gmail.com>
2025-12-25 18:59:56 +0100

    Angular - RxJS - Handling HTTP Errors with Observables
    Implements error handling for product and product list retrieval.
    
    Wraps the product service calls in the components with a catchError pipe to handle errors and prevent the application from crashing.  The error message is then displayed to the user.
    
    The product service implements a generic error handling function to format the error message.

--- Angular/RxJS_Angular_Signals/angular-rxjs-signals-fundamentals/apm-begin/src/app/products/product-list/product-list.component.ts
+++ Angular/RxJS_Angular_Signals/angular-rxjs-signals-fundamentals/apm-begin/src/app/products/product-list/product-list.component.ts
@@ -4,7 +4,7 @@ import { NgIf, NgFor, NgClass } from '@angular/common';
 import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { ProductService } from '../product.service';
-import { Subscription, tap } from 'rxjs';
+import { catchError, EMPTY, Subscription, tap } from 'rxjs';
 
 @Component({
     selector: 'pm-product-list',
@@ -28,8 +28,13 @@ export class ProductListComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.sub = this.productService.getProducts().pipe(
-      tap(() => console.log('in component pipeline'))
-    ).subscribe(products => {
+      tap(() => console.log('in component pipeline')),
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    ).subscribe(
+      products => {
       this.products = products;
       console.log(this.products);
     });
