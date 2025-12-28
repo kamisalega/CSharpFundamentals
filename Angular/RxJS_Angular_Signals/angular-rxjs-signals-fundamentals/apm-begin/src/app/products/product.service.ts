3552e4eea1567ede8c2819ffb04a7cc61e35e42e
Kamil Salega <kamilsalega@gmail.com>
2025-12-28 18:35:33 +0100

    Angular - RxJS - Getting Related Data: switchMap, concatMap and mergeMap

--- Angular/RxJS_Angular_Signals/angular-rxjs-signals-fundamentals/apm-begin/src/app/products/product.service.ts
+++ Angular/RxJS_Angular_Signals/angular-rxjs-signals-fundamentals/apm-begin/src/app/products/product.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, Observable, of, tap, throwError } from 'rxjs';
+import { catchError, map, Observable, of, switchMap, tap, throwError } from 'rxjs';
 import { Product } from './product';
-import { ProductData } from './product-data';
 import { HttpErrorService } from '../utilities/http-error.service';
+import { ReviewService } from '../reviews/review.service';
+import { Review } from '../reviews/review';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ export class ProductService {
   private productsUrl = 'api/products';
   private http = inject(HttpClient);
   private errorService = inject(HttpErrorService);
+  private reviewService = inject(ReviewService);
 
   getProducts(): Observable<Product[]>{
     return this.http.get<Product[]>(this.productsUrl).pipe(
@@ -24,10 +26,22 @@ export class ProductService {
     const productUrl = this.productsUrl + '/' + id;
     return this.http.get<Product>(productUrl).pipe(
       tap(() => console.log('In http.get by id pipeline')),
+      switchMap(product => this.getProductWithReviews(product)),
+      tap(x => console.log(x)),
       catchError(err => this.handleError(err))
     );
   }
 
+  private getProductWithReviews(product: Product): Observable<Product> {
+      if(product.hasReviews){
+        return this.http.get<Review[]>(this.reviewService.getReviewUrl(product.id))
+        .pipe(
+          map(reviews => ({ ...product, reviews} as Product) )
+        )
+      } else{
+        return of(product);
+      }
+  }
   private handleError(err: HttpErrorResponse): Observable<never> {
       const formattedMessage = this.errorService.formatError(err);
       //return throwError(() => formattedMessage);
