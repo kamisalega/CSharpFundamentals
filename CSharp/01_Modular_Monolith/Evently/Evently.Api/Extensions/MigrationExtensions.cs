518c5d36e79839ccb76ae9b14b3915048399d2b6
Kamil Salega <kamilsalega@gmail.com>
2026-01-31 08:26:46 +0100

    CSharp - ModularMonolith - Sets up initial project structure
    Adds initial files and configurations to create a modular monolith application using .NET 8.
    
    This includes:
    - Creating Dockerfiles and Docker Compose files for containerization
    - Adding .editorconfig to enforce code style
    - Configuring project properties for code analysis and .NET 8 target framework
    - Implementing Events module with basic database setup using EF Core and PostgreSQL

---
+++ CSharp/01_Modular_Monolith/Evently/Evently.Api/Extensions/MigrationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Evently.Modules.Events.Api.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Evently.Api.Extensions;
+
+internal static class MigrationExtensions
+{
+    internal static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+        ApplyMigration<EventsDBContext>(scope);
+    }
+
+    private static void ApplyMigration<TDBContext>(IServiceScope scope) where TDBContext : DbContext
+    {
+        using TDBContext context = scope.ServiceProvider.GetRequiredService<TDBContext>();
+
+        context.Database.Migrate();
+    }
+}
---
+++ CSharp/01_Modular_Monolith/Evently/Evently.Modules.Events.Api/Events/GetEvent.cs
@@ -0,0 +1,26 @@
+﻿using Evently.Modules.Events.Api.Database;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+
+namespace Evently.Modules.Events.Api.Events;
+
+public static class GetEvent
+{
+    public static void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("events/{id}", async (Guid id, EventsDBContext context) =>
+        {
+            EventResponse? @event = await context.Events.Where(e => e.Id == id)
+                .Select(e => new EventResponse(e.Id, 
+                    e.Title, 
+                    e.Description, 
+                    e.Location, 
+                    e.StartsAtUtc, 
+                    e.EndsAtUtc))
+                .SingleOrDefaultAsync();
+            return @event is not null ? Results.Ok(@event) : Results.NotFound();
+        }).WithTags(Tags.Events);
+    }
+}
