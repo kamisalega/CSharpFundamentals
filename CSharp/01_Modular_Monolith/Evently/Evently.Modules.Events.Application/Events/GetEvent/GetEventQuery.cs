fb966bae6c3486edaf15adab04e0ccecdf46327c
Kamil Salega <kamilsalega@gmail.com>
2026-02-02 08:11:57 +0100

    CSharp - ModularMonolith - Refactors Events module to Clean Architecture
    Moves the Events module towards a layered architecture, separating domain, application, infrastructure, and presentation concerns.
    
    This change facilitates better separation of concerns, improves testability, and enhances maintainability. It also sets the stage for future feature development and scalability.
    
    The module now uses MediatR for command and query handling and FluentValidation for request validation. Dapper is used for querying.
    
    Removes the old Events API module and replaces it with the new infrastructure module that utilizes EF core and a postgres database.

---
+++ CSharp/01_Modular_Monolith/Evently/Evently.Modules.Events.Application/Events/GetEvent/GetEventQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Evently.Modules.Events.Application.Events.GetEvent;
+
+public sealed record GetEventQuery(
+    Guid Id) : IRequest<EventResponse?>;
---
+++ CSharp/01_Modular_Monolith/Evently/Evently.Modules.Events.Presentation/Events/GetEvent.cs
@@ -0,0 +1,20 @@
+﻿using Evently.Modules.Events.Application.Events.GetEvent;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Evently.Modules.Events.Presentation.Events;
+
+internal static class GetEvent
+{
+    public static void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("events/{id}", async (Guid id, ISender sender) =>
+        {
+            EventResponse @event = await sender.Send(new GetEventQuery(id));
+         
+            return @event is not null ? Results.Ok(@event) : Results.NotFound();
+        }).WithTags(Tags.Events);
+    }
+}
