24b6986651c0bdc2758be0c8ca747d456fcc8043
Kamil Salega <kamilsalega@gmail.com>
2026-02-06 05:57:39 +0100

    CSharp - ModularMonolith - Module Configuration
    Configures the application to load module-specific configurations
    from JSON files, enabling modularity.
    
    Registers the Events module and adds infrastructure services.

--- CSharp/01_Modular_Monolith/Evently/Evently.Api/Program.cs
+++ CSharp/01_Modular_Monolith/Evently/Evently.Api/Program.cs
@@ -1,5 +1,6 @@
 using Evently.Api.Extensions;
 using Evently.Common.Application;
+using Evently.Common.Infrastructure;
 using Evently.Modules.Events.Infrastructure;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -10,8 +11,10 @@ builder.Services.AddSwaggerGen(options =>
 {
     options.CustomSchemaIds(t => t.FullName?.Replace("+", "."));
 });
+builder.Configuration.AddModuleConfiguration(["events"]);
 builder.Services.AddEventsModule(builder.Configuration);
 builder.Services.AddApplication([Evently.Modules.Events.Application.AssemblyReference.Assembly]);
+builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("Database")!);
 WebApplication app = builder.Build();
 
 
