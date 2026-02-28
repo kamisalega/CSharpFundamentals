383df9cc9c1f685aa0e521a2b91c885c79cbaeae
Kamil Salega <kamilsalega@gmail.com>
2026-02-28 07:18:19 +0100

    CSharp - ModularMonolith - Establishes comprehensive architectural tests
    Introduces architectural tests using NetArchTest to enforce design principles across modules and layers.
    
    Adds specific tests for:
    - Layer dependencies within modules (e.g., Users).
    - Naming conventions and accessibility for commands, queries, handlers, validators, and domain events.
    - Module independence, ensuring no direct dependencies between core module projects beyond integration events.
    
    This helps maintain the modular monolith architecture and ensures consistent code structure.

---
+++ CSharp/01_Modular_Monolith/Evently/Evently.Modules.Attendance.Application/AssemblyReference.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+namespace Evently.Modules.Attendance.Application;
+public static class AssemblyReference
+{
+    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+}
---
+++ CSharp/01_Modular_Monolith/Evently/Evently.Modules.Users.ArchitectureTest/Presentation/PresentationTests.cs
@@ -0,0 +1,31 @@
+﻿using Evently.Modules.Users.ArchitectureTest.Abstractions;
+using MassTransit;
+using NetArchTest.Rules;
+
+namespace Evently.Modules.Users.ArchitectureTest.Presentation;
+public class PresentationTests : BaseTest
+{
+    [Fact]
+    public void IntegrationEventHandler_Should_BeSealed()
+    {
+        Types.InAssembly(PresentationAssembly)
+            .That()
+            .ImplementInterface(typeof(IConsumer<>))
+            .Should()
+            .BeSealed()
+            .GetResult()
+            .ShouldBeSuccessful();
+    }
+
+    [Fact]
+    public void IntegrationEventHandler_ShouldHave_NameEndingWith_DomainEventHandler()
+    {
+        Types.InAssembly(PresentationAssembly)
+            .That()
+            .ImplementInterface(typeof(IConsumer<>))
+            .Should()
+            .HaveNameEndingWith("Consumer")
+            .GetResult()
+            .ShouldBeSuccessful();
+    }
+}
