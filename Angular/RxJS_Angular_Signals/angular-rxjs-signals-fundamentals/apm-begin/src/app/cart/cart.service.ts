b5d5dd0a324ec26f9f8eb7a9475a5f4e0a67a96d
Kamil Salega <kamilsalega@gmail.com>
2026-03-02 17:53:50 +0100

    Angular - RxJS - Calculates cart totals using signals
    Moves the logic for subtotal, delivery fee, tax, and total price calculation from `CartTotalComponent` to `CartService`.
    Leverages Angular `computed` signals in the service to provide reactive and dynamic cart total values.
    Updates the `CartTotalComponent` to inject and consume these computed signals, ensuring the UI accurately reflects cart state.

--- Angular/RxJS_Angular_Signals/angular-rxjs-signals-fundamentals/apm-begin/src/app/cart/cart.service.ts
+++ Angular/RxJS_Angular_Signals/angular-rxjs-signals-fundamentals/apm-begin/src/app/cart/cart.service.ts
@@ -12,9 +12,24 @@ export class CartService {
     this.cartItems().reduce((accQty, item) => accQty + item.quantity, 0),
   );
 
+  subTotal = computed(() =>
+    this.cartItems().reduce(
+      (accTotal, item) => accTotal + item.quantity * item.product.price,
+      0,
+    ),
+  );
+
+  deliveryFee = computed<number>(() => (this.subTotal() < 50 ? 5.99 : 0));
+  tax = computed(() => Math.round(this.subTotal() * 10.75) / 100);
+
+  totalPrice = computed(
+    () => this.subTotal() + this.deliveryFee() + this.tax(),
+  );
+
   eLenght = effect(() =>
     console.log('Cart arry lenght:', this.cartItems().length),
   );
+
   addToCart(product: Product): void {
     this.cartItems.update((items) => [...items, { product, quantity: 1 }]);
   }
