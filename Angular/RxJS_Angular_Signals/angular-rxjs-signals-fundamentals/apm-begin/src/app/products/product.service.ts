82d9e0d9801aa051c5b4333047dcea455fce6e71
Kamil Salega <kamilsalega@gmail.com>
2026-03-05 18:01:28 +0100

    Angular - RxJS - Handling Product Detail Errors with Signals
    Refactor the product detail retrieval to use the Result pattern, converting the observable stream into signals that expose both the product data and any potential error messages.

--- Angular/RxJS_Angular_Signals/angular-rxjs-signals-fundamentals/apm-begin/src/app/products/product.service.ts
+++ Angular/RxJS_Angular_Signals/angular-rxjs-signals-fundamentals/apm-begin/src/app/products/product.service.ts
@@ -29,8 +29,7 @@ export class ProductService {
 
   selectedProductId = signal<number | undefined>(undefined);
 
-
-  private products$ = this.http.get<Product[]>(this.productsUrl).pipe(
+  private productsResult$ = this.http.get<Product[]>(this.productsUrl).pipe(
     map((p) => ({ data: p }) as Result<Product[]>),
     tap((p) => console.log(JSON.stringify(p))),
     shareReplay(1),
@@ -42,24 +41,33 @@ export class ProductService {
     ),
   );
 
-  private productsResult = toSignal(this.products$, {
+  private productsResult = toSignal(this.productsResult$, {
     initialValue: { data: [] } as Result<Product[]>,
   });
   products = computed(() => this.productsResult().data);
   productsError = computed(() => this.productsResult().error);
 
-  readonly product$ = toObservable(this.selectedProductId).pipe(
+  private productResult$ = toObservable(this.selectedProductId).pipe(
     filter(Boolean),
     switchMap((id) => {
       const productUrl = this.productsUrl + '/' + id;
       return this.http.get<Product>(productUrl).pipe(
         switchMap((product) => this.getProductWithReviews(product)),
         tap((x) => console.log(x)),
-        catchError((err) => this.handleError(err)),
+        catchError((err) =>
+          of({
+            data: undefined,
+            error: this.errorService.formatError(err),
+          } as Result<Product>),
+        ),
       );
     }),
+    map((p) => ({ data: p }) as Result<Product>),
   );
 
+  private productResult = toSignal(this.productResult$);
+  product = computed(() => this.productResult()?.data);
+  productError = computed(() => this.productResult()?.error);
   // product$ = combineLatest([this.productSelected$, this.products$]).pipe(
   //   map(([selectedProductId, products]) =>
   //     products.find((product) => product.id === selectedProductId),
